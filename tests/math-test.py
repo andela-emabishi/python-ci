2040bb2af287a0d055e9695fdef86f44b7f3e531
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-03-03 10:29:30 +0300

    [Chore] Edit circle.yml file
    [Chore] Edit circle.yml file
    
    [Chore] Add coveralls post test
    
    [Chore] Add .coveralls.yml and load .env
    
    [Chore] Add coveralls pkg, Update requirements.txt
    
    [Chore] Modify circle.yml

--- tests/math-test.py
+++ tests/math-test.py
@@ -6,3 +6,5 @@ class MathTest(unittest.TestCase):
     def test_addition(self):
         # Make test fail
         self.assertEqual(Math.addition(3, 4), 8)
+        # with self.assertRaises(TypeError):
+        #     Math.addition(a, 4)
